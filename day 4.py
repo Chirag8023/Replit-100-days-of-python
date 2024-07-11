82654df9de28fc15435983ebdc2e07c4545a39b5
Cʜɪʀᴀɢ Aʀᴏʀᴀ <chirag8023@gmail.com>
2024-07-11 16:10:13 +0000

    day 4

---
+++ day 4.py
@@ -0,0 +1,9 @@
+print("\033[32m","Welcome to your adventure simulator. I am going to ask you a bunch of questions","\033[35m"," and then create an epic story with you as the star!")
+
+name=input("What is your name? ")
+worstEnemyName=input("What is your worst enemy’s name? ")
+superpower=input("What is your superpower?")
+liveAt=input("Where do you live? ")
+favoriteFood=input("What is your favorite food? ")
+
+print("\033[31m","Hello ",name,"! Your ability to ",superpower," will make sure you never have to look at ",worstEnemyName," again. Go eat ","\033[0m",favoriteFood," as you walk down the streets of ", liveAt," and use", superpower, "for good and not evil!")
